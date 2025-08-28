53d57cb8256c5c539a4fbb8327be2725d0a3462a
Ioannis Anagnostakis <412135+rizitis@users.noreply.github.com>
2025-08-28 20:00:00 +0300

    Update JsonFormatterTest.py

--- test/JsonFormatterTest.py
+++ test/JsonFormatterTest.py
@@ -1,5 +1,3 @@
-# coding=utf-8
-# encoding: utf-8
 import unittest
 import os
 import filecmp
@@ -9,7 +7,7 @@ from slacklog.parsers import SlackLogParser
 from slacklog.formatters import SlackLogJsonFormatter
 
 
-class JsonFormatterTest (unittest.TestCase):
+class JsonFormatterTest(unittest.TestCase):
 
     def setUp(self):
         self.input = './test/changelogs/'
@@ -19,15 +17,14 @@ class JsonFormatterTest (unittest.TestCase):
         self.out_encoding = 'utf-8'
         self.parser = SlackLogParser()
         self.formatter = SlackLogJsonFormatter()
-        # Try to ensure clean output dir
-        shutil.rmtree(self.output, True)
+        # Ensure clean output dir
+        shutil.rmtree(self.output, ignore_errors=True)
         os.mkdir(self.output)
 
     def tearDown(self):
-        shutil.rmtree(self.output, True)
+        shutil.rmtree(self.output, ignore_errors=True)
 
     def test(self):
-
         self.update_json("slackware",   "12.0")
         self.update_json("slackware",   "12.1")
         self.update_json("slackware",   "13.0")
@@ -42,19 +39,21 @@ class JsonFormatterTest (unittest.TestCase):
         self.update_json("slackware64", "14.1")
         self.update_json("slackware",   "14.2")
         self.update_json("slackware64", "14.2")
+        self.update_json("slackware",   "15.0")
+        self.update_json("slackware64", "15.0")
         self.update_json("slackware",   "current")
         self.update_json("slackware64", "current")
 
-        base_rss = os.listdir(self.baseline)
-        match, mismatch, error = filecmp.cmpfiles(self.baseline, self.output, base_rss, False)
+        base_files = os.listdir(self.baseline)
+        match, mismatch, error = filecmp.cmpfiles(self.baseline, self.output, base_files, shallow=False)
 
-        self.assertEqual(len(base_rss), len(match))
+        self.assertEqual(len(base_files), len(match))
         self.assertEqual(0, len(mismatch))
         self.assertEqual(0, len(error))
 
     def update_json(self, slackware, version):
         self.formatter.indent = 4
 
-        slacklog = self.parser.parse(read("%s%s-%s.txt" % (self.input, slackware, version), self.encoding))
-        unicode_text = self.formatter.format(slacklog)
-        write("%s%s-%s.json" % (self.output, slackware, version), unicode_text)
+        slacklog = self.parser.parse(read(f"{self.input}{slackware}-{version}.txt", self.encoding))
+        json_text = self.formatter.format(slacklog)
+        write(f"{self.output}{slackware}-{version}.json", json_text)
