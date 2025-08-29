95753705b133a9636bab5a5c45342980bd2b2b0c
Anagnostakis Ioannis <elahistos@yahoo.gr>
2025-08-29 18:26:44 +0300

    Fixed infos in txt files and link for webpage
    Signed-off-by: Anagnostakis Ioannis <elahistos@yahoo.gr>

--- README.rst
+++ README.rst
@@ -53,6 +53,7 @@ Current version is a fork by Ioannis Anagnostakis (rizitis):
 - Modified scripts for Python 3.9+
 - Removed Docker and tests
 - ForkHomePage `https://rizitis.github.io/slacklog/`_
+- Added python3-slacklog.SlackBuild on SBo
 
 
 License
---
+++ doc/deploy_slacklog.sh
@@ -0,0 +1,30 @@
+# Deploy Slacklog Sphinx HTML to https://rizitis.github.io/slacklog/
+
+set -e
+
+# Prevent running as root
+if [ "$EUID" -eq 0 ]; then
+    echo "ERROR: Do not run this script as root. Exiting."
+    exit 1
+fi
+
+# Paths
+SPHINX_BUILD_DIR="_build/html"
+GITHUB_PAGES_DIR="$HOME/GITHUB/rizitis.github.io/slacklog"
+
+echo "Copying HTML to GitHub Pages..."
+rm -rf "$GITHUB_PAGES_DIR"/*
+cp -r "$SPHINX_BUILD_DIR"/* "$GITHUB_PAGES_DIR"/
+
+cd "$GITHUB_PAGES_DIR"
+
+echo "We are hacking in https://rizitis.github.io so renaming _folders to avoid Jekyll issues..."
+mv _static static
+mv _sources sources
+mv _modules modules
+
+echo "Updating HTML links...because of Jekyll hacking pages https://rizitis.github.io/slackware/"
+find . -name '*.html' -exec sed -i 's|"_static/|"static/|g; s|"_sources/|"sources/|g; s|"_modules/|"modules/|g' {} +
+
+echo "Done! Now I must commit and push the changes."
+echo ""
